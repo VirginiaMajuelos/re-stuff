7c04006a9eddd253de38a835e4563602d71dd27c
Virginia Majuelos <virginia.majuelos@gmail.com>
2021-12-09 19:15:45 +0100

    crear request, a solucionar otros problemas

--- client/src/Components/pages/Forms/NewRequest.js
+++ client/src/Components/pages/Forms/NewRequest.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react'
 import { Form, Button, Container, Row } from 'react-bootstrap'
-import ProductService from '../../../services/product.service'
+import RequestService from '../../../services/request.service'
 
 export default class NewRequest extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      imageUrl: "",
-      name: "",
-      price: "",
-      description: "",
-      categorie: "",
-      cityProduct: "",
-      postCode: "",
+      inicialDate: "",
+      finalDate: "",
+      comments: "",
+      requestOwner: "",
+      isAccept: "",
+      idProduct: ""
     }
 
-    this.productService = new ProductService()
+    this.requestService = new RequestService()
   }
+
+  componentDidMount = () => this.setState({requestOwner: this.props.loggedUser?._id, idProduct: this.props.match.params.id})
 
   handleSubmit = (e) => {
     e.preventDefault();
-console.log(this.state)
-    this.productService.createProduct(this.state)
+
+  console.log(this.state)
+    this.requestService.createRequest(this.state)
       .then(response => {
         // this.props.refreshProduct()
-        console.log(response)
+        // console.log(response)
         this.props.history.push("/")
       })
       .catch(err => console.log(err))
@@ -43,38 +45,23 @@ console.log(this.state)
       <Container style={{padding:"60px"}}>
       <Row>
       <Form onSubmit={this.handleSubmit}>
-        <Form.Group className="mb-3" controlId="imageUrl">
-          <Form.Label>Url de la imagen</Form.Label>
-          <Form.Control onChange={this.handleInputChange} value={this.state.imageUrl} name="imageUrl" type="text" />
+        <Form.Group className="mb-3" controlId="inicialDate">
+          <Form.Label>Fecha de inicio</Form.Label>
+          <Form.Control onChange={this.handleInputChange} value={this.state.inicialDate} name="inicialDate" type="date" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="name">
-          <Form.Label>Name</Form.Label>
-          <Form.Control onChange={this.handleInputChange} value={this.state.name} name="name" type="text" />
+        <Form.Group className="mb-3" controlId="finalDate">
+          <Form.Label>Fecha fin</Form.Label>
+          <Form.Control onChange={this.handleInputChange} value={this.state.finalDate} name="finalDate" type="date" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="price">
-          <Form.Label>Price</Form.Label>
-          <Form.Control onChange={this.handleInputChange} value={this.state.price} name="price" type="number" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="description">
-          <Form.Label>Description</Form.Label>
-          <Form.Control onChange={this.handleInputChange} value={this.state.description} name="description" type="text" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="cityProduct">
-          <Form.Label>City Product</Form.Label>
-          <Form.Control onChange={this.handleInputChange} value={this.state.cityProduct} name="cityProduct" type="text" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="postCode">
-          <Form.Label>Post Code</Form.Label>
-          <Form.Control onChange={this.handleInputChange} value={this.state.postCode} name="postCode" type="number" />
+          <Form.Label>comments</Form.Label>
+          <Form.Control onChange={this.handleInputChange} value={this.state.comments} name="comments" type="text" />
         </Form.Group>
 
         <Button variant="primary" type="submit">
-          Submit
+          Enviar
         </Button>
       </Form>
       </Row>
